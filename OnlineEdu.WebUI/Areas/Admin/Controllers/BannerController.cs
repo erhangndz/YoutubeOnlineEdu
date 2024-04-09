45d75008d7f198301a8cc7e84c01708283106963
Erhan Gündüz <erhangndz@live.com>
2024-04-09 08:34:04 +0300

    22. ders sonu

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/BannerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.BannerDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class BannerController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+            var values = await  _client.GetFromJsonAsync<List<ResultBannerDto>>("banners");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteBanner(int id)
+        {
+            await _client.DeleteAsync($"banners/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            await _client.PostAsJsonAsync("banners", createBannerDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateBanner(int id)
+        {
+            var value = await _client.GetFromJsonAsync<UpdateBannerDto>($"banners/{id}");
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            await _client.PutAsJsonAsync("banners", updateBannerDto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
