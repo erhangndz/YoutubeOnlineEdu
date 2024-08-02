92ae6e657e316d217f8be331b410ac39baa9568d
Erhan Gündüz <erhangndz@live.com>
2024-08-02 15:02:06 +0300

    updated

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/SocialMediaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.SocialMediaDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class SocialMediaController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultSocialMediaDto>>("SocialMedias");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteSocialMedia(int id)
+        {
+            await _client.DeleteAsync("SocialMedias/" + id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult CreateSocialMedia()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            await _client.PostAsJsonAsync("SocialMedias", createSocialMediaDto);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateSocialMedia(int id)
+        {
+           
+            var value = await _client.GetFromJsonAsync<UpdateSocialMediaDto>("SocialMedias/" + id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            await _client.PutAsJsonAsync("SocialMedias", updateSocialMediaDto);
+            return RedirectToAction("Index");
+        }
+    }
+}
