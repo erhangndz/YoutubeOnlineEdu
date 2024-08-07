b3d7df2620e431abc499c9e5fe6807a32c0031a0
Erhan Gündüz <erhangndz@live.com>
2024-08-07 14:00:34 +0300

    updated

---
+++ OnlineEdu.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult Layout()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineEdu.WebUI/ViewComponents/Home/_HomeBannerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.ViewComponents.Home
+{
+    public class _HomeBannerComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
